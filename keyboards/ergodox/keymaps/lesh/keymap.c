f3b3fd2d1c4d27fffcda12924979de753fb0f63e
lesh <lesh@sysphere.org>
2016-09-15 10:58:59 +0200

    ..

--- keyboards/ergodox/keymaps/lesh/keymap.c
+++ keyboards/ergodox/keymaps/lesh/keymap.c
@@ -6,6 +6,7 @@
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
+#define CTLL 3 // media keys
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -29,6 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |ace   | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
+
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
@@ -39,20 +41,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   KC_DELT,
         LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_UP,KC_DOWN,
                                               KC_LGUI,  KC_LGUI,
-                                                              KC_HOME,
-                                               KC_SPC,KC_LGUI,KC_END,
+                                                              KC_PGUP,
+                               KC_SPC,LT(SYMB,KC_GRV),LT(SYMB,KC_PGDN),
         // right hand
              KC_BSPC,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
-             KC_BSPC     ,KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+             KC_BSPC     ,KC_N,   KC_M,   KC_COMM,KC_DOT, LT(MDIA, KC_SLSH),   KC_RSFT,
                                   KC_LEFT,  KC_RGHT,KC_LBRC,KC_RBRC,          LT(SYMB,KC_GRV),
              KC_LALT,        CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,KC_LGUI, KC_ENT
+             KC_HOME,
+        LT(SYMB,KC_END),KC_LGUI, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
- *
+` *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |Version |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
@@ -72,14 +74,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
+
+
 // SYMBOLS
 [SYMB] = KEYMAP(
-       // left hand
+       // left hand 
        M(0),   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LBRC,KC_RBRC,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LPRN,KC_RPRN,KC_TILD,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_EXLM,KC_AT,  KC_LPRN,M(1),KC_RPRN,KC_TRNS,
+       KC_TRNS,KC_HASH,KC_DLR, KC_LBRC,M(2),KC_RBRC,
+       KC_TRNS,KC_PERC,KC_CIRC,KC_LCBR,M(3),KC_RCBR,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_PGUP,KC_PGDN,
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
                                KC_TRNS,KC_TRNS,KC_TRNS,
@@ -88,11 +92,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
                 KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
        KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+                         KC_HOME,KC_END,  KC_0,    KC_EQL,  KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
+
+
+// MEDIA AND MOUSE
+[CTLL] = KEYMAP(
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                           KC_TRNS, KC_TRNS,
+                                                    KC_TRNS,
+                                  KC_TRNS, KC_TRNS, KC_TRNS,
+    // right hand
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,
+       KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS
+),
+
+
 /* Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
@@ -114,6 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
+
 // MEDIA AND MOUSE
 [MDIA] = KEYMAP(
        KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -137,8 +166,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+	[1] = ACTION_FUNCTION(1)
+    //    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+
 };
+
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    switch (id) {
+    case 1:
+      if (record->event.pressed) {
+          layer_on(CTLL);
+          add_key(KC_LCTL);
+          send_keyboard_report();
+      } else {
+          layer_off(CTLL);
+          del_key(KC_LCTL);
+          send_keyboard_report();
+      }
+      break;
+    }
+};
+
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
@@ -149,6 +198,39 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
           SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
         }
         break;
+        
+        case 1:
+        if (record->event.pressed) {
+          return MACRODOWN(D(LSHIFT), T(LPRN), T(RPRN), U(LSHIFT), T(LEFT) ,END);
+        }
+        break;
+        
+        case 2:
+        if (record->event.pressed) {
+          return MACRO(T(LCBR), T(RCBR), T(LEFT) ,END);
+        }
+        break;
+        
+        case 3:
+        if (record->event.pressed) {
+          return MACRODOWN(D(LSHIFT), T(LBRC), T(RBRC), U(LSHIFT), T(LEFT) ,END);
+        }
+        break;
+        
+        case 4:
+        if (record->event.pressed) {
+          SEND_STRING ("YOLO BURRITO HA HA HA HA");
+        }
+        break;
+        
+        case 5:
+        if (record->event.pressed) {
+          return MACRO(D(LCTL), END);
+        } else {
+          return MACRO(U(LCTL), END);
+        }
+        break;
+        
       }
     return MACRO_NONE;
 };
@@ -166,8 +248,6 @@ void matrix_scan_user(void) {
   cnt++;
   uint8_t layer = biton32(layer_state);
 
-  
-    
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
@@ -209,6 +289,9 @@ void matrix_scan_user(void) {
             break;
         case 2:
             ergodox_right_led_2_on();
+            break;
+        case 3:
+            ergodox_right_led_3_on();
             break;
         default:
           //            ergodox_right_led_3_on();
